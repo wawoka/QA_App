c783f7263aab7c077f6254db9b516da52cfcd7e7
kanamori <dorakuruse@yahoo.co.jp>
2017-09-27 17:47:39 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/atsushi/kanamori/qa_app/Const.java
@@ -0,0 +1,9 @@
+package jp.techacademy.atsushi.kanamori.qa_app;
+
+public class Const {
+    public static final String UsersPATH = "users"; // Firebaseにユーザの表示名を保存するパス
+    public static final String ContentsPATH = "contents"; // Firebaseに質問を保存するパス
+    public static final String AnswersPATH = "answers"; // Firebaseに回答を保存するパス
+
+    public static final String NameKEY = "name"; // Preferenceに表示名を保存する時のキー
+}
